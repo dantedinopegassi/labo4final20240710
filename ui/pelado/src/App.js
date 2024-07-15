24b6437eeb8603347e6065764ef68ea78a2bb4ee
dinopegassi <nmbost@gmail.com>
2024-07-15 22:34:18 +0930

    anda el front, vamo argentina carajo

--- ui/pelado/src/App.js
+++ ui/pelado/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import api from "./api";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [materias, setMaterias] = useState([]);
@@ -150,7 +151,6 @@ const App = () => {
                 <td>{materia.date}</td>
               </tr>
             ))}
-            ;
           </tbody>
         </table>
       </div>
